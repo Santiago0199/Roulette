e8e0351b206c8004e89a6a07d84de78d86d3fbac
Santiago Perdomo Forero <01santiagopf99@gmail.com>
2020-07-06 22:23:39 -0500

    se adiciona carga de properties custom

---
+++ src/main/java/com/masivian/roulette/conf/CustomPropertiesConfig.java
@@ -0,0 +1,10 @@
+package com.masivian.roulette.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:validation.properties")
+public class CustomPropertiesConfig {
+
+}
